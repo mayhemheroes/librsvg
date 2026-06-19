8a2735a9cd0f867c534d0288e3f0bfa97c4afee2
Federico Mena Quintero <federico@gnome.org>
2026-06-18 18:39:56 -0600

    Add a test for mutually-recursive CSS files
    Part-of: <https://gitlab.gnome.org/GNOME/librsvg/-/merge_requests/1200>

--- rsvg/tests/loading_crash.rs
+++ rsvg/tests/loading_crash.rs
@@ -54,4 +54,5 @@ mod tests {
     t!(xinclude_text_xml_svg,                        "xinclude-text-xml.svg");
     t!(xml_pi_without_data_svg,                      "xml-pi-without-data.svg");
     t!(bug1234_recursive_css_svg,                    "bug1234-recursive-css.svg");
+    t!(bug1234_mutually_recursive_css_svg,           "bug1234-mutually-recursive-css.svg");
 }
